370cdaa2c28cbc54dc79aa8774bea4f265ecd185
chaneyo <othman.chaney@roche.com>
2020-05-26 16:29:59 -0700

    adding proper pressure sensor lib

--- pppgui/fluidics/lib_pressure_sensor.py
+++ pppgui/fluidics/lib_pressure_sensor.py
@@ -1,16 +1,27 @@
 import random
+from serial import Serial
 from decimal import Decimal
+
 
 class pressureSensor(object):
 
     def __init__(self):
-        print('p sensor')
-
-    def connect_to_ps(self):
-        pass
+        try:
+            self.ser = Serial('/dev/USBttyx', 115200, 8, 'N', 1, 0.1, 0, 0, 0, 0)
+            print('p sensor connected')
+        except:
+            print('No Pressure Sensor Detected')
 
     def read_pressure(self):
-        x = random.random()
-        print(x)
-        return x
+        '''
+        Reads Pressure from the IDEX sensor
+        :return: Pressure in psi
+        '''
 
+        self.ser.flushInput()
+        self.ser.write('RB412\r\n')
+        # x = int(r[2:9], 16) move this back in when implamenting
+        x = random.random()  # take this out
+        pressure = ((((x * 1000) / (2 ** 22)) * 100) * 0.000145)
+        print(x)
+        return pressure
