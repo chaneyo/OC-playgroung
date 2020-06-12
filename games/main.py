0195155155df21bdec3e0d76ad3a5c9073d6735b
chaneyo <othman.chaney@roche.com>
2020-06-12 10:14:15 -0700

    adding proper CentrisPump.py need to make adjustments to make it work when on the actual CentrisPump

--- games/main.py
+++ games/main.py
@@ -4,8 +4,15 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
+#title
+pygame.display.set_caption("Othman's Invaders")
+icon = pygame.image.load('joelsm2.png')
+pygame.display.set_icon(icon)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0, 25, 20))
+    pygame.display.update()
