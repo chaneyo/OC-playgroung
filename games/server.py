37a8b9d21d8b4c5ffc6c522e358dc22314489398
chaneyo <othman.chaney@roche.com>
2020-07-05 22:28:30 -0700

    finally got it working

---
+++ games/server.py
@@ -0,0 +1,75 @@
+import socket
+import pickle
+from _thread import *
+from game import Game
+
+server = "192.168.1.144"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("this shit hasn't started")
+
+connected = set()
+games = {}
+idCount = 0
+
+
+def threaded_client(conn, p, gameId):
+    global idCount
+    conn.send(str.encode(str(p)))
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(4096).decode()
+
+            if gameID in games:
+                game = games[gameID]
+
+                if not data:
+                    break
+                else:
+                    if data == "reset":
+                        game.resetWent()
+                    elif data != "get":
+                        game.play(p, data)
+
+                    reply = game
+                    conn.sendall(pickle.dumps(reply))
+            else:
+                break
+        except:
+            break
+
+    print("Lost connection")
+    try:
+        del games[gameID]
+        print("Closing game", gameID)
+    except:
+        pass
+    idCount -= 1
+    conn.closed()
+
+
+while True:
+    conn, addr = s.accept()
+    print("connected to:", addr)
+
+    idCount += 1
+    p = 0
+    gameID = (idCount - 1) // 2
+    if idCount % 2 == 1:
+        games[gameID] = Game(gameID)
+        print("creating a new game")
+    else:
+        games[gameID].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, gameID))
